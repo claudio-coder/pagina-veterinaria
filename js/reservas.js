79dbfaf75ac7b5537494aac9f37a739934e4a320
Claudio Cabanas <ccabanas@alonenet.com.ar>
2022-09-14 21:26:57 -0300

    incorporar eventos

--- js/reservas.js
+++ js/reservas.js
@@ -1,45 +1,21 @@
-const SERVICIOS = [{
-    id: 1,
-    nombre: "Clinica General",
-},
-{
-    id: 2,
-    nombre: "Medicina Holistica",
-},
-{
-    id: 3,
-    nombre: "Metodo de Diagnostico",
-},
-{
-    id: 4,
-    nombre: "Electrocardiogramas",
-},
-{
-    id: 5,
-    nombre: "Oncología",
-},
-{
-    id: 6,
-    nombre: "Exóticos",
-},
-{
-    id: 7,
-    nombre: "Fisioterapia",
-},
-{
-    id: 8,
-    nombre: "Ecografias",
-},
-{
-    id: 9,
-    nombre: "Farmacia",
-},
-{
-    id: 10,
-    nombre: "Baños y Peluquería",
-},
+const SERVICIOS = [
+    { id: "1", nombre: "Clinica General", turnos: [], profesionales: ["Elvira"] },
+    { id: "2", nombre: "Medicina Holistica", turnos: [], profesionales: ["Patricia"] },
+    { id: "3", nombre: "Metodo de Diagnostico", turnos: [], profesionales: ["Cintya"] },
+    { id: "4", nombre: "Electro/Ecografias", turnos: [], profesionales: ["Daniel"] },
+    { id: "5", nombre: "Oncología", turnos: [], profesionales: ["Alejandra"] },
+    { id: "6", nombre: "Exóticos", turnos: [], profesionales: ["Leonardo"] },
+    { id: "7", nombre: "Fisioterapia", turnos: [], profesionales: ["Patricia"] },
+    { id: "8", nombre: "Baños y Peluquería", turnos: [], profesionales: ["Susana"] },
 
 ]
+
+if (localStorage.getItem("servicios") === null) {
+    localStorage.setItem("servicios", JSON.stringify(SERVICIOS));
+
+}
+
+
 
 
 class Reservas {
@@ -58,13 +34,16 @@ class Reservas {
 
 
         SERVICIOS.forEach(unServicio => {
-            reservas = reservas.concat(`<div class="cardreservas"><button class="button__reserva"> `)
+            reservas = reservas.concat(`<div class="cardreservas"><a href="../pages/turnos.html?id=`)
+                .concat(unServicio.id)
+                .concat(`" class="button__reserva"> `)
                 .concat(unServicio.nombre)
-                .concat(`</button>`).concat("</div>")
-        });
-        console.log(reservas);
-        return reservas;
+                .concat(`</a>`)
+                .concat("</div>")
 
+        });
+
+        return reservas;
 
 
     }
